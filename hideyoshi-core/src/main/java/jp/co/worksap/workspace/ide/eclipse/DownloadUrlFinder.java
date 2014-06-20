f7065b2f22266e65b931d6b9cc0baa4b7d46faec
Kengo TODA <toda_k@worksap.co.jp>
2014-06-20 09:56:40 +0800

    improve coverage: add test case for DownloadUrlFinder.

--- hideyoshi-core/src/main/java/jp/co/worksap/workspace/ide/eclipse/DownloadUrlFinder.java
+++ hideyoshi-core/src/main/java/jp/co/worksap/workspace/ide/eclipse/DownloadUrlFinder.java
@@ -7,24 +7,37 @@ import java.io.InputStream;
 import java.util.Properties;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import com.google.common.base.Strings;
 
 import jp.co.worksap.workspace.common.OperatingSystem;
 
+@ParametersAreNonnullByDefault
 public class DownloadUrlFinder {
 
     @Nonnull
-    String findDownloadUrl(@Nonnull Version version) throws IOException {
+    String findDownloadUrl(Version version) throws IOException {
+        OperatingSystem system = OperatingSystem.create();
+        return findDownloadUrl(version, system);
+    }
+
+    @Nonnull
+    String findDownloadUrl(Version version, OperatingSystem system) throws IOException {
         checkNotNull(version);
 
         try (InputStream urlInfo = version.loadUrlInformation()) {
+            if (urlInfo == null) {
+                throw new IllegalArgumentException("specified version (" + version + ") is not supported yet.");
+            }
+
             Properties urlInfoProperty = new Properties();
             urlInfoProperty.load(urlInfo);
-            OperatingSystem system = OperatingSystem.create();
             String key = String.format("javaee.%s.%d", system.getName(), system.getBits());
 
             String downloadUrl = urlInfoProperty.getProperty(key);
-            if (downloadUrl == null) {
-                throw new IllegalArgumentException("download URL for specified version (" + version + ") is unknown");
+            if (Strings.isNullOrEmpty(downloadUrl)) {
+                throw new UnsupportedOperationException("download URL for specified version (" + version + ") is unknown");
             }
 
             return downloadUrl;
