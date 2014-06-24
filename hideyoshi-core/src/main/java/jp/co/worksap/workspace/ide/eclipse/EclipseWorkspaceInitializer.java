49bbd8ffe508acd12c86459e438c0e41967e75bb
eller86 <skypencil@gmail.com>
2014-06-24 19:29:38 +0800

    support charset configuration for Eclipse project.

--- hideyoshi-core/src/main/java/jp/co/worksap/workspace/ide/eclipse/EclipseWorkspaceInitializer.java
+++ hideyoshi-core/src/main/java/jp/co/worksap/workspace/ide/eclipse/EclipseWorkspaceInitializer.java
@@ -1,13 +1,25 @@
 package jp.co.worksap.workspace.ide.eclipse;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
+import lombok.AllArgsConstructor;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Charsets;
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
 
 public class EclipseWorkspaceInitializer {
+    @VisibleForTesting
+    static final String PREFIX_OF_ENCODING = "encoding/<project>=";
+
     /**
      * @return directory which is workspace of Eclipse
      */
@@ -15,7 +27,7 @@ public class EclipseWorkspaceInitializer {
         ensureDirectoryExists(workspace);
 
         try {
-            createPrefFiles(workspace);
+            createPrefFiles(workspace, config.getDefaultCharset());
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
@@ -23,20 +35,38 @@ public class EclipseWorkspaceInitializer {
         new FindBugsPluginConfigurator().configure(config.getFindbugs(), workspace);
     }
 
-    private void createPrefFiles(File workspace) throws IOException {
+    private void createPrefFiles(File workspace, Charset defaultCharset) throws IOException {
         File settingDirectory = new File(workspace, ".settings");
         ensureDirectoryExists(settingDirectory);
 
         URL template = Resources.getResource(getClass(), "org.eclipse.core.resources.prefs");
         File corePrefFile = new File(settingDirectory, "org.eclipse.core.resources.prefs");
 
-        // TODO support changing encoding from UTF-8 to required one
-        Resources.asByteSource(template).copyTo(Files.asByteSink(corePrefFile));
+        FluentIterable<String> prefs = FluentIterable.from(Resources.asCharSource(template, Charsets.UTF_8).readLines());
+        Files.asCharSink(corePrefFile, Charsets.UTF_8).writeLines(
+                prefs.transform(new EncodingReplacer(defaultCharset)));
     }
 
     private void ensureDirectoryExists(File directory) {
         if (!directory.isDirectory() && !directory.mkdir()) {
             throw new IllegalStateException("could not create directory at:" + directory.getAbsolutePath());
         }
+    }
+
+    @AllArgsConstructor
+    private static final class EncodingReplacer implements Function<String, String> {
+        private final Charset charset;
+
+        @Override
+        public String apply(String before) {
+            checkNotNull(before);
+
+            if (before.startsWith(PREFIX_OF_ENCODING)) {
+                return PREFIX_OF_ENCODING + charset.displayName();
+            } else {
+                return before;
+            }
+        }
+
     }
 }
--- hideyoshi-core/src/test/java/jp/co/worksap/workspace/ide/eclipse/EclipseWorkspaceInitializerTest.java
+++ hideyoshi-core/src/test/java/jp/co/worksap/workspace/ide/eclipse/EclipseWorkspaceInitializerTest.java
@@ -1,30 +1,66 @@
 package jp.co.worksap.workspace.ide.eclipse;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.mockito.Mockito;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 public class EclipseWorkspaceInitializerTest {
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
-    @Test
-    public void workspaceInitializationShouldCreatePrefFile() throws IOException {
-        EclipseWorkspaceInitializer initializer = new EclipseWorkspaceInitializer();
-        File workspace = folder.newFolder();
-        File settingDirectory = new File(workspace, ".settings");
-        File prefFile = new File(settingDirectory, "org.eclipse.core.resources.prefs");
+    private EclipseWorkspaceInitializer initializer;
+    private File workspace;
+    private File settingDirectory;
+    private File prefFile;
+
+    @Before
+    public void setUp() throws IOException {
+        initializer = new EclipseWorkspaceInitializer();
+        workspace = folder.newFolder();
+        settingDirectory = new File(workspace, ".settings");
+        prefFile = new File(settingDirectory, "org.eclipse.core.resources.prefs");
 
         assertThat(settingDirectory.exists(), is(false));
         assertThat(prefFile.exists(), is(false));
+    }
+
+    @Test
+    public void workspaceInitializationShouldCreatePrefFile() throws IOException {
         initializer.initialize(new EclipseConfiguration(), workspace);
+
         assertThat(settingDirectory.isDirectory(), is(true));
         assertThat(prefFile.isFile(), is(true));
+    }
+
+    @Test
+    public void workspaceInitializationUsesProperEncoding() throws IOException {
+        EclipseConfiguration configuration = Mockito.spy(new EclipseConfiguration());
+        doReturn(Charset.forName("SHIFT_JIS")).when(configuration).getDefaultCharset();
+
+        initializer.initialize(configuration, workspace);
+        assertThat(findCharset(), is(equalToIgnoringCase("SHIFT_JIS")));
+    }
+
+    private String findCharset() throws IOException {
+        for (String line : Files.readLines(prefFile, Charsets.UTF_8)) {
+            if (line.startsWith(EclipseWorkspaceInitializer.PREFIX_OF_ENCODING)) {
+                return line.substring(EclipseWorkspaceInitializer.PREFIX_OF_ENCODING.length());
+            }
+        }
+        throw new IllegalStateException("no encoding found");
     }
 }
